0f4ebd7195e9e8be523464935d5ecd3448ad0ad3
Amirov Mikhail <amirov@kontur.ru>
2018-01-14 23:36:20 +0500

    Simple server added.

---
+++ MessengerBackend/MessengerBackend/EntryPoint.cs
@@ -0,0 +1,13 @@
+﻿using MessengerBackend.ServiceSettings;
+
+namespace MessengerBackend
+{
+    public class EntryPoint
+    {
+        public static void Main()
+        {
+            var settings = new MessengerServiceSettings();
+            var server = new SimpleHTTPServer(settings.Folder, settings.Port);
+        }
+    }
+}
---
+++ MessengerBackend/MessengerBackend/ServiceSettings/MessengerServiceSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace MessengerBackend.ServiceSettings
+{
+    public class MessengerServiceSettings
+    {
+        public int Port = 31777;
+        public string Folder = "C:/MessengerWorkFolder";
+    }
+}
